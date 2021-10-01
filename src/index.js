4155ec126bf08af6d457740b97aa5f2780968fe8
Cletus Dias <cletus6@gmail.com>
2021-10-01 12:31:59 -0400

    commented code where appropriate

--- src/index.js
+++ src/index.js
@@ -3,21 +3,23 @@ import calcResult from '../lib/calcResult.js';
 
 export default async function getTask() {
 	try {
+		// fetch data from the url that gives id, operation, left, right values
 		const res = await axios.get('https://interview.adpeai.com/api/v1/get-task');
 		const { id, operation, left, right } = res.data;
 		if (id) {
+			// if id exixts run our utility function calResult to calculate result based on the type of operation
 			const result = calcResult(operation, left, right);
 			const data = {
 				id,
 				result,
 			};
-			console.log({ ...res.data, result });
+			// make a post request with the data
 			const postResult = await axios.post(
 				'https://interview.adpeai.com/api/v1/submit-task',
 				data
 			);
-			// console.log(postResult.data);
-			return postResult.data;
+			console.log({ ...res.data, result });
+			// return postResult.data;
 		}
 	} catch (error) {
 		console.log(error.data);
